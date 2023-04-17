93547055ccd4c7323eb28ec143617205b044a7c9
Leyla <luzhakh@gmail.com>
2023-04-17 21:51:05 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// alert("hellowww");
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("тебе есть 18?", "");
+// console.log(answer + 5);
+
+// let und;
+
+// const obj = {
+//   name: "Leka",
+//   age: 25,
+//   isMarried: true,
+// };
+
+// console.log(obj["name"]);
+
+// let arr = ["plum.png", "orange.jpeg", 6, "apple.bmp"];
+// console.log(arr[1]);
+
+// const answers = [];
+
+// answers[0] = prompt("whats your name?", "");
+// answers[1] = prompt("whats your surname?", "");
+// answers[2] = prompt("how old are you", "");
+
+// document.write(answers);
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Hello, ${user}`);
+
+const isChecked = true,
+  isClose = false;
+
+console.log(isChecked || isClose);
